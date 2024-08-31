cfcea6f237e7210e8f02ee001ee82c996dead661
DengShuo <dengshuo346@hotmail.com>
2024-08-31 12:00:17 +0800

    feat(colormodel):add color model

--- src/utils/colormodelrenderer.ts
+++ src/utils/colormodelrenderer.ts
@@ -1,42 +1,51 @@
-import { Vector3, Color } from 'three'
+import { Vector3 } from 'three'
 import { wavelengthData } from './wavelength'
 
 export class ColorModelGenerator {
-  // Constructor can be empty or take any necessary parameters
-  constructor() {}
+  private wavelengthMap: Map<number, { r: number; g: number; b: number }>
+  private wavelengthRange: { min: number; max: number }
+  private sortedWavelengths: number[]
 
-  // Method to generate a color model (e.g., RGB cube)
-  public generateRGBCube(size: number = 1): Vector3[] {
-    const points: Vector3[] = []
-    for (let r = 0; r <= 1; r += 0.1) {
-      for (let g = 0; g <= 1; g += 0.1) {
-        for (let b = 0; b <= 1; b += 0.1) {
-          points.push(new Vector3(r * size, g * size, b * size))
-        }
-      }
+  constructor() {
+    this.wavelengthMap = new Map(
+      Object.entries(wavelengthData).map(([key, value]) => [parseInt(key), value])
+    )
+    const wavelengths = Array.from(this.wavelengthMap.keys())
+    this.wavelengthRange = {
+      min: Math.min(...wavelengths),
+      max: Math.max(...wavelengths)
     }
-    return points
+    this.sortedWavelengths = Array.from(this.wavelengthMap.keys()).sort((a, b) => a - b)
   }
 
-  // Method to convert wavelength to RGB color
-  public wavelengthToRGB(wavelength: number): Color {
-    // Implement wavelength to RGB conversion logic here
-    // This is a placeholder and needs to be implemented
-    return new Color()
+  getWavelengthRange(): { min: number; max: number } {
+    return this.wavelengthRange
   }
 
-  // Method to generate 3D axes
-  public generate3DAxes(size: number = 1): Vector3[][] {
-    const axes: Vector3[][] = [
-      // X-axis (red)
-      [new Vector3(0, 0, 0), new Vector3(size, 0, 0)],
-      // Y-axis (green)
-      [new Vector3(0, 0, 0), new Vector3(0, size, 0)],
-      // Z-axis (blue)
-      [new Vector3(0, 0, 0), new Vector3(0, 0, size)]
-    ]
-    return axes
+  wavelengthToSensitivity(wavelength: number): Vector3 {
+    const sensitivity = this.interpolateWavelength(wavelength)
+    return new Vector3(sensitivity.r, sensitivity.g, sensitivity.b)
   }
 
-  // Add other methods for generating different color models or color-related calculations
+  private interpolateWavelength(wavelength: number): { r: number; g: number; b: number } {
+    const lowerIndex = this.sortedWavelengths.findIndex((w) => w >= wavelength) - 1
+    const upperIndex = lowerIndex + 1
+
+    if (lowerIndex < 0) return this.wavelengthMap.get(this.sortedWavelengths[0])!
+    if (upperIndex >= this.sortedWavelengths.length)
+      return this.wavelengthMap.get(this.sortedWavelengths[this.sortedWavelengths.length - 1])!
+
+    const lowerWavelength = this.sortedWavelengths[lowerIndex]
+    const upperWavelength = this.sortedWavelengths[upperIndex]
+    const t = (wavelength - lowerWavelength) / (upperWavelength - lowerWavelength)
+
+    const lower = this.wavelengthMap.get(lowerWavelength)!
+    const upper = this.wavelengthMap.get(upperWavelength)!
+
+    return {
+      r: lower.r + (upper.r - lower.r) * t,
+      g: lower.g + (upper.g - lower.g) * t,
+      b: lower.b + (upper.b - lower.b) * t
+    }
+  }
 }
